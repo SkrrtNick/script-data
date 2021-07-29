63c4142a167ccc6e76447f535b3ccaff7aeaa635
SkrrtNick <79165336+SkrrtNick@users.noreply.github.com>
2021-07-29 15:02:14 +0100

    Update barcrawler.js

--- models/barcrawler.js
+++ models/barcrawler.js
@@ -42,7 +42,7 @@ module.exports.getSessions = (callback) => {
 }
 
 module.exports.getUserData = (condition, callback) => {
-    barCrawler.aggregate([
+    BarCrawler.aggregate([
         {$match: condition},
         {
             $group: {
