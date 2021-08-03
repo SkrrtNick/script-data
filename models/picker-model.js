1f5d6ef241f6d020aab325b667c54adff9bbb908
SkrrtNick <79165336+SkrrtNick@users.noreply.github.com>
2021-08-03 15:27:11 +0100

    added picker

---
+++ models/picker-model.js
@@ -0,0 +1,67 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+const shortid = require('shortid')
+
+const PickerSchema = new Schema({
+    sessionId: {
+        type: String,
+        required: true,
+        default: shortid.generate
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    runtime: {
+        type: Number,
+        required: true
+    },
+    itemsPicked: {
+        type: Number,
+        required: true
+    },
+    profit: {
+        type: Number,
+        required: true
+    },
+}, {
+    timestamps: true
+});
+
+PickerSchema.pre('save', function (next) {
+    this.usernameLower = this.username.toLowerCase()
+    next();
+});
+
+let Picker = module.exports = mongoose.model('picker', PickerSchema)
+
+
+module.exports.createSession = (picker, callback) => {
+    picker.save(callback);
+}
+
+module.exports.getSessions = (callback) => {
+    picker.find({}, callback)
+}
+
+module.exports.getUserData = (condition, callback) => {
+    Picker.aggregate([
+        { $match: condition },
+        {
+            $group: {
+                _id: "$username",
+                runtime: {
+                    $sum: "$runtime"
+                },
+                itemsPicked: {
+                    $sum: "$itemsPicked"
+                },
+                profit: {
+                    $sum: "$profit"
+                }
+            }
+        }
+    ], callback)
+}
